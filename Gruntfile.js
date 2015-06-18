6ab6841e0920472adfc20a050e69133ff25c962f
Andrew Carlson <andrewc0101@gmail.com>
2015-06-17 21:29:25 -0400

    adding coveralls coverage

--- Gruntfile.js
+++ Gruntfile.js
@@ -14,14 +14,16 @@ module.exports = function(grunt) {
       options: {
 
       }
+    },
+    coveralls: {
+      src: 'coverage/lcov.info'
     }
   });
 
   grunt.loadNpmTasks('grunt-contrib-jshint');
   grunt.loadNpmTasks('grunt-mocha-istanbul');
-
+  grunt.loadNpmTasks('grunt-coveralls');
 
   grunt.registerTask('default', ['jshint','mocha_istanbul']);
-
 
 };
